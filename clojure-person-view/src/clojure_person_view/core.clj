f3be9a71916a588843c7aa86a99745853b49b4ff
Anders Karlsen <karlsenanders@hotmail.com>
2012-04-11 00:11:05 +0200

    Started clojure webpage generation

--- clojure-person-view/src/clojure_person_view/core.clj
+++ clojure-person-view/src/clojure_person_view/core.clj
@@ -1,9 +1,27 @@
 (ns clojure-person-view.core
+(:use [hiccup.core])  
+(:use [hiccup.page])  
+(:use [hiccup.form])  
 (:gen-class
-   :methods [[hello [String] String]]))
+   :methods [[hello [String] String]])
+)
    
  
 (defn -hello
   [this s]
   (str "Hello, " s))
+
+(defn generate-create-page []
+  (html5 [:body 
+          (form-to [:post "createPerson.html"]
+               (text-field "first_name" "")
+               (text-field "last_name" "")
+               (text-field "last_name" "")
+               (text-field "birth_date" "")
+               (submit-button "Create person"))
+          
+          ])
+          )
+
+
 
--- clojure-person-view/test/clojure_person_view/test/core.clj
+++ clojure-person-view/test/clojure_person_view/test/core.clj
@@ -1,6 +1,15 @@
 (ns clojure-person-view.test.core
   (:use [clojure-person-view.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  )
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest generate-create-page-test
+  (let [actual (generate-create-page)]
+  (is (.contains actual "<form") actual)
+  (is (.contains actual "action=\"createPerson.html\"") actual)
+  (is (.contains actual "name=\"first_name\"") actual)
+  (is (.contains actual "name=\"last_name\"") actual)
+  (is (.contains actual "name=\"birth_date\"") actual)
+  )
+  )
+
